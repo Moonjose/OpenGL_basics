7acdaaeb65fef4e9594fc0d3dc0c490a67936f8c
Jose Antônio <ajose9975@gmail.com>
2025-09-16 16:43:15 -0300

    adiciona texturas ao shader

--- Basics.cpp
+++ Basics.cpp
@@ -2,6 +2,7 @@
 #include <GLFW/glfw3.h> 
 #include <iostream>
 #include "Shader.h"
+#include "stb_image.h"
 
 enum PrimitiveShape { Triangle, Rectangle };
 
@@ -15,9 +16,12 @@ struct PrimitiveBuffers {
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 PrimitiveBuffers setupPrimitive(PrimitiveShape shape);
+unsigned int loadTexture(const char* path);
 
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
+
+unsigned int texture1, texture2;
 
 int main()
 {
@@ -46,7 +50,14 @@ int main()
 
     Shader ourShader("3.3.shader.vs", "3.3.shader.fs");
 
-    PrimitiveBuffers buffers = setupPrimitive(Triangle);
+    texture1 = loadTexture("resources/container.jpg");
+    texture2 = loadTexture("resources/awesomeface.png");
+
+    PrimitiveBuffers buffers = setupPrimitive(Rectangle);
+
+    ourShader.use();
+    glUniform1i(glGetUniformLocation(ourShader.ID, "texture1"), 0); 
+    glUniform1i(glGetUniformLocation(ourShader.ID, "texture2"), 1);
 
     while (!glfwWindowShouldClose(window))
     {
@@ -54,16 +65,22 @@ int main()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture1);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture2);
+
+        ourShader.use();
+
         if (buffers.useEBO) {
-            ourShader.use();
             glBindVertexArray(buffers.VAO[0]);
             glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         }
         else {
-            ourShader.use();;
             glBindVertexArray(buffers.VAO[0]);
             glDrawArrays(GL_TRIANGLES, 0, 3);
         }
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
@@ -74,6 +91,42 @@ int main()
 
     glfwTerminate();
     return 0;
+}
+
+unsigned int loadTexture(const char* path)
+{
+    unsigned int textureID;
+    glGenTextures(1, &textureID);
+
+    int width, height, nrChannels;
+    stbi_set_flip_vertically_on_load(true); 
+    unsigned char* data = stbi_load(path, &width, &height, &nrChannels, 0);
+    if (data)
+    {
+        GLenum format = GL_RGB;
+        if (nrChannels == 1)
+            format = GL_RED;
+        else if (nrChannels == 3)
+            format = GL_RGB;
+        else if (nrChannels == 4)
+            format = GL_RGBA;
+
+        glBindTexture(GL_TEXTURE_2D, textureID);
+        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    }
+    else
+    {
+        std::cout << "Failed to load texture: " << path << std::endl;
+    }
+    stbi_image_free(data);
+
+    return textureID;
 }
 
 void processInput(GLFWwindow* window)
@@ -91,25 +144,17 @@ PrimitiveBuffers setupPrimitive(PrimitiveShape shape) {
     PrimitiveBuffers buffers = {};
     buffers.useEBO = false;
 
-    float firstTriangle[] = {
-        // positions         // colors
-         0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
-        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
-         0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top 
+    float triangleVertices[] = {
+         0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,
+        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,
+         0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f
     };
 
-    //float secondTriangle[] = {
-    //     0.0f, -0.5f, 0.0f,
-    //     0.9f, -0.5f, 0.0f,
-    //     0.45f, 0.5f, 0.0f
-    //};
-
     float rectangleVertices[] = {
-        // positions         // colors
-         0.5f,  0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // top right (red)
-         0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // bottom right (green)
-        -0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f,  // bottom left (blue)
-        -0.5f,  0.5f, 0.0f,  1.0f, 1.0f, 0.0f   // top left (yellow)
+         0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,
+         0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,
+        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,
+        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f
     };
 
     unsigned int rectangleIndices[] = {
@@ -121,24 +166,29 @@ PrimitiveBuffers setupPrimitive(PrimitiveShape shape) {
     glGenBuffers(2, buffers.VBO);
 
     auto setupVAO = [shape](unsigned int vao, unsigned int vbo, const float* data, size_t size) {
+        float stride = (shape == Triangle) ? 6 * sizeof(float) : 8 * sizeof(float);
+
         glBindVertexArray(vao);
         glBindBuffer(GL_ARRAY_BUFFER, vbo);
         glBufferData(GL_ARRAY_BUFFER, size, data, GL_STATIC_DRAW);
 
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, stride, (void*)0);
         glEnableVertexAttribArray(0);
 
-        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, stride, (void*)(3 * sizeof(float)));
         glEnableVertexAttribArray(1);
+
+        if (shape == Rectangle) {
+            glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, stride, (void*)(6 * sizeof(float)));
+            glEnableVertexAttribArray(2);
+        }
     };
 
     if (shape == Triangle) {
-        setupVAO(buffers.VAO[0], buffers.VBO[0], firstTriangle, sizeof(firstTriangle));
-        //setupVAO(buffers.VAO[1], buffers.VBO[1], secondTriangle, sizeof(secondTriangle));
+        setupVAO(buffers.VAO[0], buffers.VBO[0], triangleVertices, sizeof(triangleVertices));
     }
     else {
         setupVAO(buffers.VAO[0], buffers.VBO[0], rectangleVertices, sizeof(rectangleVertices));
-
         buffers.useEBO = true;
         glGenBuffers(1, &buffers.EBO);
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, buffers.EBO);
